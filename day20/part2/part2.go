3d414b00de672b53664df837a3d1e6c48bc131e4
Nick Boughton <nicholasboughton@googlemail.com>
2015-12-24 19:39:41 +0000

    added part2 solution

--- day20/part2/part2.go
+++ day20/part2/part2.go
@@ -9,8 +9,6 @@ func main() {
 	var house, presents int
 
 	// houses 2,4,8,16 and all primes etc = n-1 * 2 + 10
-	// Increment houses by 60 as multiples of 60 produce the
-	// biggest step change in present numbers
 	for house = 0; house != -1; house += 60 {
 		presents = getsPresents(house)
 		if presents >= input {
@@ -22,8 +20,8 @@ func main() {
 func getsPresents(house int) int {
 	presents := 0
 	for elf := 1; elf <= house; elf++ {
-		if house%elf == 0 {
-			presents += 10 * elf
+		if house%elf == 0 && house/elf <= 50 {
+			presents += 11 * elf
 		}
 	}
 	fmt.Printf("house %v gets %v presents\n", house, presents)
