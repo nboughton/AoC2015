1e3e64d35e7577fa35352b39d4aff1a8cdbea874
Nick Boughton <nicholasboughton@googlemail.com>
2015-12-24 19:21:48 +0000

    day 20 part1 solution done

---
+++ day20/part1/part1.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	input := 34000000
+	var house, presents int
+
+	// houses 2,4,8,16 and all primes etc = n-1 * 2 + 10
+	for house = 0; house != -1; house += 60 {
+		presents = getsPresents(house)
+		if presents >= input {
+			break
+		}
+	}
+}
+
+func getsPresents(house int) int {
+	presents := 0
+	for elf := 1; elf <= house; elf++ {
+		if house%elf == 0 {
+			presents += 10 * elf
+		}
+	}
+	fmt.Printf("house %v gets %v presents\n", house, presents)
+	return presents
+}
